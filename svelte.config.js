41a86c508a2b2f4ada600a8f97c1bab5615b836d
yehudz <yehuda.zeytim@gmail.com>
2022-08-10 15:23:30 -0600

    style: Font initialization

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
-
+import path from "path";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -12,7 +12,7 @@ const config = {
   ],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	}
 };
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,31 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontSize: {
+      sm: ['18px', {fontWeight: '300'}],
+      'md-light': ['24px', {fontWeight: '300'}],
+      md: ['24px', {fontWeight: '500'}],
+      lg: ['32px', {fontWeight: '300'}],
+      'body-m': ['15px', {fontWeight: '300'}],
+      'body-s': ['13px', {fontWeight: '300'}],
+    },
+    colors: {
+      'red': '#FC4747',
+      'black': '#10141E',
+      'light-grey': '#5A698F',
+      'dark-grey': '#161D2F',
+      'white': '#FFFFFF',
+    },
+    fontFamily: {
+      sans: ['Outfit'],
+      serif: ['sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
