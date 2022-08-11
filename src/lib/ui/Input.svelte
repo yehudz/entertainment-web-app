58906a8dc83f03cbb3578ce134cceccedb9eec64
yehudz <yehuda.zeytim@gmail.com>
2022-08-10 18:08:23 -0600

    feat: Input component

---
+++ src/lib/ui/Input.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let placeholder: string = ''
+  export let inputType: string = 'text'
+  let value: string = ''
+  let error: boolean = false
+  let inputElement: HTMLInputElement
+  
+  onMount(()=> {
+    inputElement.type = inputType
+  })
+
+  function checkInputValidation() {
+    if (!value) emptyInput()
+  }
+
+  function removeError() {
+    if (value) {
+      error = false
+      inputElement.classList.remove('error')
+    }
+  }
+
+  function emptyInput() {
+    error = true
+    inputElement.classList.add('error')
+  }
+</script>
+
+<div class="input-container relative flex items-center text-body-m">
+  <input 
+    on:keyup={removeError}
+    on:blur={()=> checkInputValidation()}
+    bind:value={value}
+    bind:this={inputElement} 
+    type="text" 
+    placeholder={placeholder} 
+    class="input py-4 text-white pl-4"
+  >
+  {#if error}
+    <div class="text-red absolute top-4 right-4 text-body-m">Can't be empty</div>
+  {/if}
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,7 @@
 <script>
-  import Searchbar from "$lib/ui/Searchbar.svelte";
+  import Input from "$lib/ui/Input.svelte";
   import "../styles/app.scss";
 </script>
 
 <slot />
-<Searchbar />
+<Input placeholder="Email Address" inputType="email"/>
