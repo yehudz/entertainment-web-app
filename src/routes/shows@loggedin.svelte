8508a61f1cc46a75d8025dea8892406bb16d4dbb
Yehuda Zeytim <yehuda.zeytim@gmail.com>
2022-08-21 19:53:18 +0300

    update: Search in tv shows functionality (#7)
    Users can search for tv shows inside the respective section

--- src/routes/shows@loggedin.svelte
+++ src/routes/shows@loggedin.svelte
@@ -3,6 +3,23 @@
   import AllContent from '$lib/views/AllContent.svelte'
   import Searchbar from '$lib/ui/Searchbar.svelte';
   let series = data.filter(item=>item.category === 'TV Series')
+
+  let searchQuery = ''
+
+  function onChange(e: any) {
+    searchQuery = e.target.value
+  }
+  
+  $: searchResults = data.filter(item=> {
+    if (!searchQuery) return item
+    else if (
+      item.title
+      .toLowerCase()
+      .includes(searchQuery
+      .toLowerCase())) 
+      return item
+  })
+
 </script>
 <div 
   data-testid="shows-section"
@@ -10,10 +27,19 @@
 >
   <Searchbar 
     placeholderText="Search for TV series"
+    {onChange}
   />
   <AllContent 
-    movies={series}
-    sectionTitle="TV Series"
+    movies={
+      !searchQuery ? 
+      series : 
+      searchResults}
+    sectionTitle={
+      !searchQuery ? 
+      "TV series" : 
+      `Found ${searchResults.length} 
+      results for 
+      "${searchQuery}"`
+    }
   />
-/>
 </div>
