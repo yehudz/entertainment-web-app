55d286ba7c6d690d3fc001cf36261acfffc25401
yehudz <yehuda.zeytim@gmail.com>
2022-08-21 10:41:47 -0600

    update: Add search functionality to movies section
    Functionality for search should be as the user input types.

--- src/routes/movies@loggedin.svelte
+++ src/routes/movies@loggedin.svelte
@@ -3,6 +3,22 @@
   import AllContent from '$lib/views/AllContent.svelte'
   import Searchbar from '$lib/ui/Searchbar.svelte';
   let movies = data.filter(item=>item.category === 'Movie')
+
+  let searchQuery = ''
+
+  function onChange(e: any) {
+    searchQuery = e.target.value
+  }
+  
+  $: searchResults = data.filter(item=> {
+    if (!searchQuery) return item
+    else if (
+      item.title
+      .toLowerCase()
+      .includes(searchQuery
+      .toLowerCase())) 
+      return item
+  })
 </script>
 <div 
   data-testid="movies-section"
@@ -10,10 +26,21 @@
 >
   <Searchbar 
     placeholderText="Search for movies"
+    {onChange}
+
   />
   <AllContent 
-    movies={movies}
-    sectionTitle="Movies"
+    movies={ 
+      !searchQuery ? 
+      movies : 
+      searchResults
+    }
+    sectionTitle={
+      !searchQuery ? 
+      "Movies" : 
+      `Found ${searchResults.length} 
+      results for 
+      "${searchQuery}"`
+    }
   />
-/>
 </div>
